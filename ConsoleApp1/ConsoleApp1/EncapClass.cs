2db9c273d462f479bef5e59d2587c4139150d015
Lithara Perera <litharaperera2002@gmail.com>
2023-04-19 23:25:46 +0530

    1

---
+++ ConsoleApp1/ConsoleApp1/EncapClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class EncapClass
+    {
+        private int age;
+        private string name;
+        public void setAge(int ageFromUser)
+        {
+            age = ageFromUser;
+        }
+
+        public int getAge() 
+        { 
+            return age; 
+        }
+
+        public void setName(string nameFromUser)
+        {
+            name = nameFromUser;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EncapClass ec = new EncapClass();
+
+            ec.setAge(25);
+            Console.WriteLine("Your age is " +  ec.getAge());
+            Console.ReadLine();
+
+            ec.setName("Lithara");
+            Console.WriteLine("Your name is " + ec.getName());
+            Console.ReadLine();
+        }
+    }
+}
