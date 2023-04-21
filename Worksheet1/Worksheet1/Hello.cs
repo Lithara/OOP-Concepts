60c43d5b74afefecd2039812c4452a6659deec53
Lithara Perera <litharaperera2002@gmail.com>
2023-04-21 15:27:33 +0530

    Worksheet Question 01

---
+++ Worksheet1/Worksheet1/Hello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet1
+{
+    internal class Hello
+    {
+        public int a = 100;
+    }
+}
---
+++ Worksheet1/Worksheet1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello hello = new Hello();
+            Console.WriteLine("Your age is: " +hello.a);
+            Console.ReadLine();
+        }
+    }
+}
