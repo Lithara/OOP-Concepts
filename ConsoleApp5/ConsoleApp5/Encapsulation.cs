9c55737ddeb00f8d83c81339a3029899455502bb
Lithara Perera <litharaperera2002@gmail.com>
2023-04-29 14:25:26 +0530

    exercise02

---
+++ ConsoleApp5/ConsoleApp5/Encapsulation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Encapsulation
+    {
+        private double num1;
+        private double num2;
+
+        public double Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+
+        public double Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+
+        public double Sum()
+        {
+            return num1 + num2;
+        }
+
+        public double Sub()
+        {
+            return num1 - num2;
+        }
+
+        public double Mul()
+        {
+            return num1 * num2;
+        }
+
+        public double Div()
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Encapsulation objEncapsulation = new Encapsulation();
+
+            Console.Write("Enter the first value : ");
+            int num1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second value : ");
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Summation : " + objEncapsulation.Sum());
+            Console.WriteLine("Subtraction : " + objEncapsulation.Sub());
+            Console.WriteLine("Multiplication : " + objEncapsulation.Mul());
+            Console.WriteLine("Division : " + objEncapsulation.Div());
+
+            Console.ReadKey();
+        }
+    }
+}
